5beac3460fc10996a8fbab30537d9568838744e3
AtmanShah <talk2atman@gmail.com>
2016-10-09 15:28:14 +0530

    Speed Change
    The Following Change Will Remove Query Strings From Static Resources. This is considered as a speed modification, useful thing for improving speed.

--- functions.php
+++ functions.php
@@ -700,3 +700,11 @@ if ( ! function_exists('skin_logo_upload_area') ) {
 			</div>
 	<?php }
 }
+
+function remove_cssjs_ver( $src ) {
+if( strpos( $src, '?ver=' ) )
+$src = remove_query_arg( 'ver', $src );
+return $src;
+				  }
+add_filter( 'style_loader_src', 'remove_cssjs_ver', 10, 2 );
+add_filter( 'script_loader_src', 'remove_cssjs_ver', 10, 2 );
